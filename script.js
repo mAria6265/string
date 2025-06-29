b93274e2f3bee1f19a965c22702e52bcbd4f87df
mAria6265 <maria.lucia.fonseca@escola.pr.gov.br>
2025-06-29 17:04:05 +0000

    commit 5.5 (aula 6)

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-
-  const palavrasChave = processaTexto(texto);
-
-  campoResultado.textContent = palavrasChave.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
-  let palavras = texto.split(/\P{L}+/u);
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-  const frequencias = contaFrequencias(palavras);
-
-  let frequencias = {};
-
-  for (let i of palavras) {
-    frequencias[i] = 0;
-
-    for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
-      }
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
     }
-  }
 
-  console.log(frequencias);
-
-  return palavras;
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
 }
 
 function contaFrequencias(palavras) {
-  let frequencias = {};
-
-  for (let i of palavras) {
-    frequencias[i] = 0;
-
-    for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
-      }
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
     }
-  }
-
-  return frequencias;
+    return frequencias;
 }
